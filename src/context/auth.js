ea72d176f6aa9604ba40ef677910f2d46c435485
Beers15 <alexanderbeers15@gmail.com>
2021-12-06 20:06:14 -0800

    Modify Auth Context to user API server instead of local memory for login and register.

--- src/context/auth.js
+++ src/context/auth.js
@@ -1,15 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import cookie from 'react-cookies';
-import jwt from 'jsonwebtoken';
-
-/* This app doesn't have a backend, to include a DB. Registered users 
-   will be added to this object, which doesn't persist on app restart
-   TODO: add bcrypt for passwords and persist to localstorage */
-const userPool = {
-  admin: {password:'password', username: 'admin', role:'admin', capabilities:['create','read','update','delete']},
-  editor: { password: 'password', username: 'editor', role: 'editor', capabilities: ['read', 'create', 'update']},
-  writer: { password: 'password', username: 'writer', role: 'writer', capabilities: ['create', 'read']},
-};
+import axios from 'axios';
 
 export const AuthContext = React.createContext();
 
@@ -19,25 +10,27 @@ const Auth = ({children}) => {
 
   useEffect(() => {
     const qs = new URLSearchParams(window.location.search);
-    const cookieToken = cookie.load('auth');
-    const token = qs.get('token') || cookieToken || null;
-    validateToken(token);
+    cookie.save('auth', qs.get('token') || cookie.load('auth') || null);
   }, []);
 
-  const can = (capability) => {
+  const can = capability => {
     return values?.user?.capabilities?.includes(capability);
   }
 
-  const login = (username, password) => {
-    if(userPool[username]) {
-      //Create a "good" token, like you'd get from a server
-      const token = jwt.sign(userPool[username], process.env.REACT_APP_SECRET || 'SECRET');
-      let user = validateToken(token);
-      if(user && userPool[username].password === password) {
-        setLoginState(true, token, user);
-      }
-    } else {
-      console.log("INVALID LOGIN");
+  const login = async (username, password) => {
+    if(username === '' || password === '') {
+      return false;
+    }
+
+    try {
+      let res = await axios.post('https://api-integration-server.herokuapp.com/signin', {},  {
+        auth: {
+          username,
+          password
+        }
+      });
+      setLoginState(true, res.data.user);
+    } catch(err) {
       return false;
     }
   }
@@ -46,18 +39,26 @@ const Auth = ({children}) => {
     setLoginState(false, null, {});
   };
 
-  const register = (username, password) => {
-    if(userPool[username] || password === '' || username === '') {
+  const register = async (username, password) => {
+    if(username === '' || password === '') {
       return false;
-    } else {
-      userPool[username] = { username, password, role:'admin', capabilities:['create','read','update','delete'] }
-      const token = jwt.sign(userPool[username], process.env.REACT_APP_SECRET || 'SECRET');
-      let user = validateToken(token);
-      if(user) {
-        setLoginState(true, token, user);
-      }
+    }
+
+    try {
+      let res = await axios.post('https://api-integration-server.herokuapp.com/signup', { username, password, role: "admin" });
+      console.log(res, 'in register')
+      setLoginState(true, res.data.user);
+    } catch(err) {
+      console.log(err);
+      return false;
     }
   }
+
+  const setLoginState = (loggedIn, user) => {
+    cookie.save('auth', user?.token);
+    setLoggedIn(loggedIn);
+    setUser(user);
+  };
 
   let values = {
     loggedIn,
@@ -66,30 +67,6 @@ const Auth = ({children}) => {
     login,
     logout,
     register,
-  };
-
-  const validateToken = token => {
-    try {
-      let user = jwt.verify(token, process.env.REACT_APP_SECRET || 'SECRET');
-      if(user) {
-        return user;
-      } else { 
-        return false;
-      }
-      
-    }
-    catch(e) {
-      setLoginState(false, null, {});
-      console.log('Token Validation Error', e);
-      return false;
-    }
-  };
-
-  const setLoginState = (loggedIn, token, user) => {
-    cookie.save('auth', token);
-    setLoggedIn(loggedIn);
-    setUser(user);
-    //setToken(token);
   };
 
   return (
